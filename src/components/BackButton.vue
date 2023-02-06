556e407f40365042a3ff9ec609a0ca4777f7b5bf
Yegor Knyazev <egorknyazev10@gmail.com>
2023-02-06 18:32:51 +0400

    add button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,22 @@
+<!-- Кнопка назад -->
+
+<template>
+    <v-row @click="router.back()" class="back-button">
+			<v-icon icon="mdi-arrow-left" />
+			<div>Назад</div>
+		</v-row>
+</template>
+
+<script setup>
+import router from '@/router'
+</script>
+
+<style lang="scss" scoped>
+.back-button {
+  display: flex;
+  align-items: center;
+  gap: '6px';
+  padding: '10px'; 
+  cursor: 'pointer';
+}
+</style>
