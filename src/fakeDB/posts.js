17b914c3ecc28e075f1c1578fb47ac8be5b50096
Yegor Knyazev <egorknyazev10@gmail.com>
2023-02-09 20:56:14 +0400

    added urls to posts

--- src/fakeDB/posts.js
+++ src/fakeDB/posts.js
@@ -2,12 +2,14 @@ export default [
   {
     title: 'Самые лучшие школы Глазова',
     description: 'Проблема поступления будущего ученика в школу является очень серьёзной, и, конечно, всем родителям хотят, чтобы их ребёнок получал качественное образование. Здесь вы узнаете про лучшие школы г. Глазова.\n\nСамые лучшие - Гимназия №8, ФМЛ и т. д. т. п.',
-    photos: [''], //URLs
+    photos: ['https://avatars.mds.yandex.net/get-altay/4530524/2a00000179e64be106f8902aa9d99c6d4baf/XXL', 
+      'https://krd.ru/upload/resize_cache/webp/resize_cache/iblock/423/1284_720_2/hrc3e0ls55o59d8k9r489p1akh1m74zt.webp'], //URLs
   },
   {
     title: 'Причины сухости рук у малыша',
     description: 'Lorem ipsum',
-    photos: [''],
+    photos: ['https://dszn.ru/uploads/cache/news/d9a2cbc8-1603369873.jpg/1f85548c8f02aedc74176b421d269be8',
+      'https://admmuji.ru/blog/wp-content/uploads/2022/04/a1258dd952929b90503dc3ee119865b1.jpg'],
   },
 ]
 
