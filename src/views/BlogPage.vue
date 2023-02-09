03265417ac601056a36f7fbff242522da8e7e978
Yegor Knyazev <egorknyazev10@gmail.com>
2023-02-09 21:21:25 +0400

    added posts in blog

--- src/views/BlogPage.vue
+++ src/views/BlogPage.vue
@@ -1,9 +1,30 @@
 <script setup>
-
+import posts from '@/fakeDB/posts';
 </script>
 
 <template>
-    blog
+	<v-container>
+		<v-row>
+			<v-col 
+				v-for="post in posts" 
+				:key="post"
+				lg="6"
+			>
+				<v-card 
+					:title="post.title"
+					:text="post.description"
+				>
+					<v-carousel>
+						<v-carousel-item 
+							v-for="url in post.photos" 
+							:key="url"
+							:src="url"
+						/>
+					</v-carousel>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <style lang="scss" scoped>
