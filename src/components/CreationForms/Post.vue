6885bb5d84379ac4c9377ddaf76961d143751b40
Yegor Knyazev <egorknyazev10@gmail.com>
2023-02-11 21:47:28 +0400

    added Post.vue

---
+++ src/components/CreationForms/Post.vue
@@ -0,0 +1,61 @@
+<script setup>
+let props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: Array,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  promo: {
+    type: Boolean,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  photos: {
+    type: Array,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <v-card>
+    <v-card-item>
+      <v-card-title>{{ props.title }}</v-card-title>
+      <v-card-subtitle>{{ props.subtitle }}</v-card-subtitle>
+      <v-row no-gutters class="gap-5">
+        <div v-for="tag in props.tags" :key="tag" class="topic">{{ tag }}</div>
+      </v-row>
+      <div>{{ props.description }}</div>
+      <v-carousel hide-delimiters>
+        <v-carousel-item v-for="url in props.photos" :key="url" :src="url" />
+      </v-carousel>
+    </v-card-item>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+.topic {
+  padding: 3px 10px;
+  font-size: 14px;
+  border: 1px solid rgb(var(--v-theme-accent));
+  border-radius: 99px;
+}
+</style>
