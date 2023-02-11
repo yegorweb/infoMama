36c9384818c6878839a7a5b561b8cf61a0e22082
Yegor Knyazev <egorknyazev10@gmail.com>
2023-02-11 21:17:26 +0400

    changed posts in fakeDB

--- src/fakeDB/posts.js
+++ src/fakeDB/posts.js
@@ -1,24 +1,44 @@
 export default [
   {
     title: 'Самые лучшие школы Глазова',
+    subtitle: 'В какую школу отдать ребенка?',
+    tags: ['Образование', 'Выбор'],
+    date: '2022-08-01T20:33.121-04:00',
+    author: '6d6f5gs45d54ewf',
+    promo: true,
     description: 'Проблема поступления будущего ученика в школу является очень серьёзной, и, конечно, всем родителям хотят, чтобы их ребёнок получал качественное образование. Здесь вы узнаете про лучшие школы г. Глазова.\n\nСамые лучшие - Гимназия №8, ФМЛ и т. д. т. п.',
     photos: ['https://avatars.mds.yandex.net/get-altay/4530524/2a00000179e64be106f8902aa9d99c6d4baf/XXL',
       'https://krd.ru/upload/resize_cache/webp/resize_cache/iblock/423/1284_720_2/hrc3e0ls55o59d8k9r489p1akh1m74zt.webp'], //URLs
   },
   {
     title: 'Причины сухости рук у малыша',
+    subtitle: 'Как вылечить сухие ручки?',
+    tags: ['Здоровье'],
+    date: '2023-01-23T10:20.345-05:00',
+    author: '2fg7fd8f8f890',
+    promo: false,
     description: 'Lorem ipsum',
     photos: ['https://dszn.ru/uploads/cache/news/d9a2cbc8-1603369873.jpg/1f85548c8f02aedc74176b421d269be8',
       'https://admmuji.ru/blog/wp-content/uploads/2022/04/a1258dd952929b90503dc3ee119865b1.jpg'],
   },
   {
     title: 'Самые лучшие школы Глазова',
+    subtitle: 'В какую школу отдать ребенка?',
+    tags: ['Образование', 'Выбор'],
+    date: '2022-08-01T20:33.121-04:00',
+    author: '6d6f5gs45d54ewf',
+    promo: true,
     description: 'Проблема поступления будущего ученика в школу является очень серьёзной, и, конечно, всем родителям хотят, чтобы их ребёнок получал качественное образование. Здесь вы узнаете про лучшие школы г. Глазова.\n\nСамые лучшие - Гимназия №8, ФМЛ и т. д. т. п.',
     photos: ['https://avatars.mds.yandex.net/get-altay/4530524/2a00000179e64be106f8902aa9d99c6d4baf/XXL',
       'https://krd.ru/upload/resize_cache/webp/resize_cache/iblock/423/1284_720_2/hrc3e0ls55o59d8k9r489p1akh1m74zt.webp'], //URLs
   },
   {
     title: 'Причины сухости рук у малыша',
+    subtitle: 'Как вылечить сухие ручки?',
+    tags: ['Здоровье'],
+    date: '2023-01-23T10:20.345-05:00',
+    author: '2fg7fd8f8f890',
+    promo: false,
     description: 'Lorem ipsum',
     photos: ['https://dszn.ru/uploads/cache/news/d9a2cbc8-1603369873.jpg/1f85548c8f02aedc74176b421d269be8',
       'https://admmuji.ru/blog/wp-content/uploads/2022/04/a1258dd952929b90503dc3ee119865b1.jpg'],
@@ -28,6 +48,11 @@ export default [
 /* BASE
 {
   title: '',
+  subtitle: '',
+  tags: [''],
+  date: '',
+  author: '',
+  promo: false,
   description: '',
   photos: [''],
 },
