68be4607581766fcdf51a43a8c466dadd3892a7b
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-01 21:33:33 +0400

    done post

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -7,11 +7,11 @@ let props = defineProps({
     type: Object,
     required: true,
   },
-  // Base see in @/fakeDB/posts.js
+  // Base of post see in @/fakeDB/posts.js
 });
 
-let current_time = Date.now();
-let post_time = props.post.date;
+let current_time = new Date(Date.now())
+let post_time = new Date(props.post.date)
 let delta = current_time - post_time
 
 function getPostDate() {
@@ -20,36 +20,35 @@ function getPostDate() {
     day: "numeric",
   }
 
-  if ((new Date(current_time)).getFullYear() != (new Date(post_time)).getFullYear()) {
+  if (current_time.getFullYear() != post_time.getFullYear()) {
     options.year = "numeric"
   }
 
-  return (new Date(post_time)).toLocaleString("ru", options)
+  return post_time.toLocaleString("ru", options)
 }
 
 let user = users.find(user => user.id === props.post.author)
-
-let view_more = ref(false)
 </script>
 
 <template>
   <v-card class="h-100">
     <v-card-item>
 
-      <div class="mb-3 d-flex align-center cursor-pointer">
+      <div class="d-flex flex-row justify-start align-center cursor-pointer">
         <v-avatar :image="user.avatar" />
         
         <!-- FUTURE: заменить div на router-link -->
         <div class="ml-3">
-          <span class="font-weight-black">{{ user.firstName + ' ' + user.lastName }}</span>
-          <span class="text-caption ml-2">{{ '@' + user.nickname }}</span>
-          <span class="text-caption ml-2">*</span>
-          <span class="text-caption ml-2">{{ getPostDate() }}</span>
+          <div class="d-flex flex-row justify-start lh-1 align-center">
+            <span class="font-weight-black">{{ user.firstName + ' ' + user.lastName }}</span>
+            <span class="text-caption ml-2">{{ '@' + user.nickname }}</span>
+          </div>
+          <span class="text-caption lh-1">{{ getPostDate() }}</span>
         </div>
       </div>
 
       <!-- Tags -->
-      <div class="mt-1 flex-wrap">
+      <div class="mt-2 flex-wrap">
         <v-chip
           v-for="tag in props.post.tags"
           :key="tag"
@@ -63,35 +62,12 @@ let view_more = ref(false)
       </div>
 
       <!-- Description -->
-      <Transition name="slide-fade">
-        <div v-if="view_more">
-          {{ props.post.description }}
-        </div>
-      </Transition>
-      <Transition name="slide-fade">
-        <div v-if="!view_more">
-          {{
-            props.post.description.length > 50
-              ? props.post.description.slice(0, 50) + "..."
-              : props.post.description
-          }}
-        </div>
-      </Transition>
-      <div class="d-flex justify-end">
-        <v-icon
-          :icon="
-            view_more ? 'mdi-chevron-double-up' : 'mdi-chevron-double-down'
-          "
-          class="w-100 pt-1 pb-1 justify-end"
-          color="primary"
-          style="cursor: pointer"
-          v-if="props.post.description.length >= 50"
-          @click="view_more = !view_more"
-        />
+      <div class="mt-4">
+        {{ props.post.description }}
       </div>
 
       <!-- Photos -->
-      <v-carousel :class="view_more ? '' : 'mt-4'" :show-arrows="false" height="auto">
+      <v-carousel class="mt-4" :show-arrows="false" height="auto">
         <v-carousel-item
           v-for="url in props.post.photos"
           :key="url"
